678435bba020e00173c27537fc50abcba83d927e
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-02-09 06:23:18 +0000

    program 2

---
+++ p2original.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int input_side()
+{
+  int n;
+  printf("enter the value\n");
+  scanf("%d",&n);
+}
+int check_scalene(int a,int b,int c)
+{ int result;
+    if(a==b || b==c || a==c)
+    {
+      result=1;
+    } 
+     else
+  {
+    result=0;
+   }
+   return result;
+}
+void output(int a , int b, int c, int isscalene)
+{
+  if(isscalene==0)
+ {
+ printf("triangle is scalene\n");
+ }
+  else
+  {
+  printf("triangle is not a scalene \n");
+  }
+}
+int main()
+{
+  int a, b, c, isscalene;
+  a = input_side();
+  b=input_side();
+  c=input_side();
+  isscalene= check_scalene(a,b,c);
+  output(a,b,c,isscalene);
+  return 0 ;
+}
